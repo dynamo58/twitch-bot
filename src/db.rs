050cde70d23b6864aea26b2c0dba315cc067fbf8
dynamo58 <63768027+dynamo58@users.noreply.github.com>
2022-02-28 11:26:16 +0100

    yikes

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use sqlx::{Connection, Sqlite, SqliteConnection};
 
 // generate tables for Markov chains
-pub async fn index_tables_for_markov(client: &mut SqliteConnection)
+pub async fn index_tables_for_markov(db: &mut SqliteConnection)
 -> anyhow::Result<()> {
     let sql = r#"
         SELECT 
@@ -14,7 +14,7 @@ pub async fn index_tables_for_markov(client: &mut SqliteConnection)
     "#;
 
     let tables = sqlx::query::<Sqlite>(sql)
-        .execute(&mut *client)
+        .execute(&mut *db)
         .await?
         .split(" ").collect::<&str>();
     
@@ -26,14 +26,61 @@ pub async fn index_tables_for_markov(client: &mut SqliteConnection)
     "#;
     
     for table_name in tables {
+        // TODO: remake `try_create_table` such that it takes a literal, not a 
+        try_create_table(&pool, channel).await?;
+
         let mut count = sqlx::query::<Sqlite>(sql)
             .bind(table_name)
-            .execute(&mut *client)
+            .execute(&mut *db)
             .await?
             .parse::(u32).unwrap();
 
+        let mut offset = 0;
         while count != 0 {
-            let 
+            let messages = sqlx::query::<Sqlite>("SELECT message FROM $1 LIMIT=100 OFFSET $2;")
+                .bind(table_name)
+                .bind(offset)
+                .execute(&mut *db)
+                .await?
+                .parse::(Vec<&str>).unwrap();
+
+                for msg in messages {
+                    let words = msg.split(" ");
+
+                    for idx in words.len() {
+                        let pred = match idx {
+                            0 => "",
+                            _ => words[idx - 1],
+                        };
+                        let word = words[idx];
+                        let succ = match idx {
+                            words.len() - 1 => "",
+                            _ => words[idx + 1];
+                        }
+
+                        // sqlx::query::<Sqlite>("INSERT INTO  FROM $1 LIMIT=100 OFFSET $2;")
+                        //     .bind(table_name)
+                        //     .bind(offset)
+                        //     .execute(&mut *db)
+                        //     .await?
+                        //     .parse::(Vec<&str>).unwrap();
+
+
+                        // TODO 
+                        // tohle je absolutní clusterfuck
+                        // https://www.sqlitetutorial.net/sqlite-attach-database/
+
+                    }
+
+                }
+            
+                
+                if messages.len() < 100 {
+                    break;
+                }
+
+                offset += 100;
+                count -= 100;
         }
         
         
